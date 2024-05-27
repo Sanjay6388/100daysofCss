d4db221a099bcaee3d03f85f23db2a1c1907cda1
Sanjay Sahani <95739823+Sanjay6388@users.noreply.github.com>
2024-05-27 17:03:31 +0530

    day

---
+++ Day_70/script.js
@@ -0,0 +1,7 @@
+const grower = document.querySelector(".grower");
+const select = document.querySelector("select");
+
+select.addEventListener("change", ({ target }) => {
+  grower.style.animationDirection = target.value;
+
+});
